175caa02b69f8ded06ec82d01c5b971d14d7b950
kainino0x <kainino1@gmail.com>
2024-10-23 18:46:20 +0000

    Deploying to gh-pages from  @ c891901ddc5d7fe20d3f3b0fc10e25db33a94b7f 🚀

--- sample/alphaToCoverage/emulatedAlphaToCoverage.ts
+++ sample/alphaToCoverage/emulatedAlphaToCoverage.ts
@@ -22,30 +22,31 @@ type Mask = number;
  */
 export const kEmulatedAlphaToCoverage = {
   'Apple M1 Pro': `\
-    fn emulatedAlphaToCoverage(alpha: f32, x: u32, y: u32) -> u32 {
-      let i = (y % 2) * 2 + (x % 2);
-      if (alpha < 0.5 / 16) { return ${0b0000}; }
-      // FIXME returning values out of an array is not working, always returns 0
-      if (alpha <  1.5 / 16) { return array(${0b0001}u, ${0b0000}, ${0b0000}, ${0b0000})[i]; }
-      if (alpha <  2.5 / 16) { return array(${0b0001}u, ${0b0000}, ${0b0000}, ${0b0001})[i]; }
-      if (alpha <  3.5 / 16) { return array(${0b0001}u, ${0b0001}, ${0b0000}, ${0b0001})[i]; }
-      if (alpha <  4.5 / 16) { return array(${0b0001}u, ${0b0001}, ${0b0001}, ${0b0001})[i]; }
-      if (alpha <  5.5 / 16) { return array(${0b1001}u, ${0b0001}, ${0b0001}, ${0b0001})[i]; }
-      if (alpha <  6.5 / 16) { return array(${0b1001}u, ${0b0001}, ${0b0001}, ${0b1001})[i]; }
-      if (alpha <  7.5 / 16) { return array(${0b1001}u, ${0b1001}, ${0b0001}, ${0b1001})[i]; }
-      if (alpha <  8.5 / 16) { return array(${0b1001}u, ${0b1001}, ${0b1001}, ${0b1001})[i]; }
-      if (alpha <  9.5 / 16) { return array(${0b1011}u, ${0b1001}, ${0b1001}, ${0b1001})[i]; }
-      if (alpha < 10.5 / 16) { return array(${0b1011}u, ${0b1001}, ${0b1001}, ${0b1011})[i]; }
-      if (alpha < 11.5 / 16) { return array(${0b1011}u, ${0b1011}, ${0b1001}, ${0b1011})[i]; }
-      if (alpha < 12.5 / 16) { return array(${0b1011}u, ${0b1011}, ${0b1011}, ${0b1011})[i]; }
-      if (alpha < 13.5 / 16) { return array(${0b1111}u, ${0b1011}, ${0b1011}, ${0b1011})[i]; }
-      if (alpha < 14.5 / 16) { return array(${0b1111}u, ${0b1011}, ${0b1011}, ${0b1111})[i]; }
-      if (alpha < 15.5 / 16) { return array(${0b1111}u, ${0b1111}, ${0b1011}, ${0b1111})[i]; }
+    fn emulatedAlphaToCoverage(alpha: f32, xy: vec2u) -> u32 {
+      let u = xy.x % 2u;
+      let v = xy.y % 2u;
+      // BUG: returning values out of an array is not working, always returns 0
+      if (alpha <  0.5 / 16) { return ${0b0000}; }
+      if (alpha <  1.5 / 16) { return array(array(${0b0001}u, ${0b0000}), array(${0b0000}, ${0b0000}))[v][u]; }
+      if (alpha <  2.5 / 16) { return array(array(${0b0001}u, ${0b0000}), array(${0b0000}, ${0b0001}))[v][u]; }
+      if (alpha <  3.5 / 16) { return array(array(${0b0001}u, ${0b0001}), array(${0b0000}, ${0b0001}))[v][u]; }
+      if (alpha <  4.5 / 16) { return array(array(${0b0001}u, ${0b0001}), array(${0b0001}, ${0b0001}))[v][u]; }
+      if (alpha <  5.5 / 16) { return array(array(${0b1001}u, ${0b0001}), array(${0b0001}, ${0b0001}))[v][u]; }
+      if (alpha <  6.5 / 16) { return array(array(${0b1001}u, ${0b0001}), array(${0b0001}, ${0b1001}))[v][u]; }
+      if (alpha <  7.5 / 16) { return array(array(${0b1001}u, ${0b1001}), array(${0b0001}, ${0b1001}))[v][u]; }
+      if (alpha <  8.5 / 16) { return array(array(${0b1001}u, ${0b1001}), array(${0b1001}, ${0b1001}))[v][u]; }
+      if (alpha <  9.5 / 16) { return array(array(${0b1011}u, ${0b1001}), array(${0b1001}, ${0b1001}))[v][u]; }
+      if (alpha < 10.5 / 16) { return array(array(${0b1011}u, ${0b1001}), array(${0b1001}, ${0b1011}))[v][u]; }
+      if (alpha < 11.5 / 16) { return array(array(${0b1011}u, ${0b1011}), array(${0b1001}, ${0b1011}))[v][u]; }
+      if (alpha < 12.5 / 16) { return array(array(${0b1011}u, ${0b1011}), array(${0b1011}, ${0b1011}))[v][u]; }
+      if (alpha < 13.5 / 16) { return array(array(${0b1111}u, ${0b1011}), array(${0b1011}, ${0b1011}))[v][u]; }
+      if (alpha < 14.5 / 16) { return array(array(${0b1111}u, ${0b1011}), array(${0b1011}, ${0b1111}))[v][u]; }
+      if (alpha < 15.5 / 16) { return array(array(${0b1111}u, ${0b1111}), array(${0b1011}, ${0b1111}))[v][u]; }
       return ${0b1111};
     }
   `.trimEnd(),
   'NVIDIA GeForce RTX 3070': `\
-    fn emulatedAlphaToCoverage(alpha: f32, x: u32, y: u32) -> u32 {
+    fn emulatedAlphaToCoverage(alpha: f32, xy: vec2u) -> u32 {
       if (alpha < 0.5 / 4) { return ${0b0000}; }
       if (alpha < 1.5 / 4) { return ${0b1000}; }
       if (alpha < 2.5 / 4) { return ${0b1001}; }
